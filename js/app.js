70f4215ca4a84942b7892f77d061c4424a88464a
Doryyyyyyyyyyy <vzaremba@edenschool.fr>
2025-02-11 17:24:42 +0100

    Update 2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const gameCell01 = document.querySelector(".game-cell[data-x='0'][data-y='1']")
+const gameCells = document.querySelectorAll(".game-cell")
 
 
 // Sélectionner toutes les gameCell via un querySelectorAll
@@ -7,14 +8,18 @@ const gameCell01 = document.querySelector(".game-cell[data-x='0'][data-y='1']")
 // Changer de joueur
 // Mettre à jour le gameBoard
 
-const redCircle = `<img src="./assets/Red Circle.svg" />`
+const redCircle = `<img src="./assets/counter-red-large.svg" />`
 const yellowCircle = `<img src="./assets/Yellow Circle.svg" />`
 
 gameCell01.addEventListener("click", function() {
     gameCell01.innerHTML = redCircle
 })
 
-
+gameCells.forEach(function($gameCell) {
+    $gameCell.addEventListener("click", function() {
+        $gameCell.innerHTML = redCircle
+    })
+})
 let gameBoard = [
     ["", "", "", "", "", "", ""],
     ["", "", "", "", "", "", ""],
@@ -24,3 +29,18 @@ let gameBoard = [
     ["", "", "", "", "", "", ""],
     ["", "", "", "", "", "", ""]
 ]
+
+const x = 0
+
+  gameCells.forEach(function($gameCell) {
+    $gameCell.addEventListener("click", function() {
+
+        for (let i = 5; i >= 0; i--) {
+            if (gameBoard[i][x] === "") {
+              console.log("C'est vide");
+            } else {
+              console.log("C'est pas vide")
+            }
+          }
+    })
+  })
