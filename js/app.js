95e7ad4a87076b58b281e2885269a83039564c8f
Doryyyyyyyyyyy <vzaremba@edenschool.fr>
2025-02-13 13:28:14 +0100

    Update 3

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-const gameCell01 = document.querySelector(".game-cell[data-x='0'][data-y='1']")
-const gameCells = document.querySelectorAll(".game-cell")
-
+const gameCells = document.querySelectorAll(".game-cell");
 
 // Sélectionner toutes les gameCell via un querySelectorAll
 // Ecouter les évenements
@@ -8,39 +6,49 @@ const gameCells = document.querySelectorAll(".game-cell")
 // Changer de joueur
 // Mettre à jour le gameBoard
 
-const redCircle = `<img src="./assets/counter-red-large.svg" />`
-const yellowCircle = `<img src="./assets/Yellow Circle.svg" />`
+const redCircle = `<img src="./assets/counter-red-large.svg" />`;
+const yellowCircle = `<img src="./assets/counter-yellow-large.svg" />`;
 
-gameCell01.addEventListener("click", function() {
-    gameCell01.innerHTML = redCircle
-})
 
-gameCells.forEach(function($gameCell) {
-    $gameCell.addEventListener("click", function() {
-        $gameCell.innerHTML = redCircle
-    })
-})
+let currentPlayer = "r"
 let gameBoard = [
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""]
-]
+  ["", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", ""]
+];
 
-const x = 0
 
-  gameCells.forEach(function($gameCell) {
-    $gameCell.addEventListener("click", function() {
 
-        for (let i = 5; i >= 0; i--) {
-            if (gameBoard[i][x] === "") {
-              console.log("C'est vide");
-            } else {
-              console.log("C'est pas vide")
-            }
-          }
-    })
-  })
+gameCells.forEach(function ($gameCell) {
+  $gameCell.addEventListener("click", function () {
+    const dataX = $gameCell.getAttribute("data-x");
+
+    for (let i = 5; i >= 0; i--) {
+      if (gameBoard[i][dataX] === "") {
+        console.log("C'est vide");
+
+        gameBoard[i][dataX] = currentPlayer
+
+        const $selectedCell = document.querySelector(`.game-cell[data-x='${dataX}'][data-y='${i}']`)
+        
+
+        if (currentPlayer === "r") {
+          $selectedCell.innerHTML = redCircle
+          currentPlayer = "y"
+        } else {
+          $selectedCell.innerHTML = yellowCircle
+          currentPlayer = "r"
+        }
+ 
+        return;
+      } else {
+        console.log("C'est pas vide" + gameBoard[i][dataX]);
+      }
+    }
+
+
+  });
+});
